381ee7bcf47f2017b7074c6c37c9642d5b4fa1a1
Esteban Selvaggi <selvaggi.esteban@gmail.com>
2024-04-22 22:19:38 -0300

    Primer commit

---
+++ assets/js/wabutton.js
@@ -0,0 +1,13 @@
+$(function () {
+  $("#WAButton").floatingWhatsApp({
+    phone: "5491153323937",
+    headerTitle: "Contact us through WhatsApp", // Popup Title
+    popupMessage: "How can we help you?", // Popup Message
+    showPopup: true, // Enables popup display
+    buttonImage:
+      '<img src="https://static.whatsapp.net/rsrc.php/v3/yP/r/rYZqPCBaG70.png" />', // Button Image
+    // Header color: 'crimson', // Custom header color
+    // Background color: 'crimson', // Custom background button color
+    position: "right",
+  });
+});
